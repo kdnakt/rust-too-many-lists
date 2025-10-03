04c645bd300490d25c9a5b0246fa14700a137cfd
kdnakt <a.kid.1985@gmail.com>
2025-10-04 00:02:07 +0900

    test push and pop

--- src/first.rs
+++ src/first.rs
@@ -76,4 +76,19 @@ mod tests {
             _ => panic!("Expected first node"),
         }
     }
+
+    #[test]
+    fn push_and_pop() {
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+        list.push(4);
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
 }
