830a6aed518454f0ab603b283d11c8824300815d
kdnakt <a.kid.1985@gmail.com>
2025-10-17 00:42:20 +0900

    test iter_mut()

--- src/second.rs
+++ src/second.rs
@@ -50,6 +50,12 @@ impl<T> List<T> {
             next: self.head.as_deref(),
         }
     }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut {
+            next: self.head.as_deref_mut(),
+        }
+    }
 }
 
 impl<'a, T> Iterator for Iter<'a, T> {
@@ -59,6 +65,17 @@ impl<'a, T> Iterator for Iter<'a, T> {
         self.next.map(|node| {
             self.next = node.next.as_deref();
             &node.elem
+        })
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.elem
         })
     }
 }
@@ -179,5 +196,25 @@ mod tests {
         assert_eq!(iter.next(), Some(&2));
         assert_eq!(iter.next(), Some(&1));
         assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        let next = iter.next();
+        assert_eq!(next, Some(&mut 2));
+        next.map(|value| *value = 42);
+        assert_eq!(iter.next(), Some(&mut 1));
+        assert_eq!(iter.next(), None);
+        let mut iter2 = list.iter_mut();
+        assert_eq!(iter2.next(), Some(&mut 3));
+        assert_eq!(iter2.next(), Some(&mut 42));
+        assert_eq!(iter2.next(), Some(&mut 1));
+        assert_eq!(iter2.next(), None);
     }
 }
