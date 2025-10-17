3840be17a079d275839249767e1a8b22df52c639
kdnakt <a.kid.1985@gmail.com>
2025-10-18 00:10:40 +0900

    use Rc

---
+++ src/third.rs
@@ -0,0 +1,14 @@
+use std::rc::Rc;
+
+#[derive(Debug)]
+pub struct List<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Rc<Node<T>>>;
+
+#[derive(Debug)]
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
