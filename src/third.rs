a7afe1176a13f8b1119b508b79c47e83606ad34b
kdnakt <a.kid.1985@gmail.com>
2025-10-19 13:11:38 +0900

    head and tail

--- src/third.rs
+++ src/third.rs
@@ -12,3 +12,51 @@ struct Node<T> {
     elem: T,
     next: Link<T>,
 }
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn prepend(&self, elem: T) -> List<T> {
+        List {
+            head: Some(Rc::new(Node {
+                elem,
+                next: self.head.clone(),
+            })),
+        }
+    }
+
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn tail(&self) -> List<T> {
+        List {
+            head: self.head.as_ref().and_then(|node| node.next.clone()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn basics() {
+        let list = List::new();
+        assert_eq!(list.head(), None);
+
+        let list = list.prepend(1).prepend(2).prepend(3);
+        assert_eq!(list.head(), Some(&3));
+
+        let tail = list.tail();
+        assert_eq!(tail.head(), Some(&2));
+
+        let tail2 = tail.tail();
+        assert_eq!(tail2.head(), Some(&1));
+
+        let tail3 = tail2.tail();
+        assert_eq!(tail3.head(), None);
+    }
+}
