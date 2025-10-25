ec46dc38a7078e43784c3e2783dbe79ed2c8f359
kdnakt <a.kid.1985@gmail.com>
2025-10-25 22:48:58 +0900

    push_front()

--- src/fourth.rs
+++ src/fourth.rs
@@ -13,6 +13,21 @@ impl<T> List<T> {
             tail: None,
         }
     }
+
+    fn push_front(&mut self, elem: T) {
+        let new_node = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                old_head.borrow_mut().prev = Some(new_node.clone());
+                new_node.borrow_mut().next = Some(old_head);
+                self.head = Some(new_node);
+            }
+            None => {
+                self.tail = Some(new_node.clone());
+                self.head = Some(new_node);
+            }
+        }
+    }
 }
 
 type Link<T> = Option<Rc<RefCell<Node<T>>>>;
@@ -38,6 +53,18 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        let _list: List<i32> = List::new();
+        let mut list: List<i32> = List::new();
+        assert!(list.head.is_none());
+        assert!(list.tail.is_none());
+        list.push_front(1);
+        assert!(list.head.is_some());
+        assert!(list.head.as_ref().unwrap().borrow().elem == 1);
+        assert!(list.tail.is_some());
+        assert!(list.tail.as_ref().unwrap().borrow().elem == 1);
+        list.push_front(2);
+        assert!(list.head.is_some());
+        assert!(list.head.as_ref().unwrap().borrow().elem == 2);
+        assert!(list.tail.is_some());
+        assert!(list.tail.as_ref().unwrap().borrow().elem == 1);
     }
 }
