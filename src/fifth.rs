dcdbabbb18760846e9f4069e6e9a54498ba55eb8
kdnakt <a.kid.1985@gmail.com>
2025-11-01 02:40:00 +0900

    push()

--- src/fifth.rs
+++ src/fifth.rs
@@ -1,7 +1,7 @@
 
-pub struct List<T> {
+pub struct List<'a, T> {
     head: Link<T>,
-    tail: Link<T>,
+    tail: Option<&'a mut Node<T>>,
 }
 
 type Link<T> = Option<Box<Node<T>>>;
@@ -9,4 +9,30 @@ type Link<T> = Option<Box<Node<T>>>;
 struct Node<T> {
     elem: T,
     next: Link<T>,
+}
+
+impl<'a, T> List<'a, T> {
+    pub fn new() -> Self {
+        List {
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn push(&'a mut self, elem: T) {
+        let new_tail = Box::new(Node { elem, next: None });
+
+        let new_tail = match self.tail.take() {
+            Some(old_tail) => {
+                old_tail.next = Some(new_tail);
+                old_tail.next.as_deref_mut()
+            }
+            None => {
+                self.head = Some(new_tail);
+                self.head.as_deref_mut()
+            }
+        };
+
+        self.tail = new_tail;
+    }
 }
