cee49974eb1847bd536fe4446bf6b6500f98f7d6
kdnakt <a.kid.1985@gmail.com>
2025-11-05 00:34:16 +0900

    new layout with *mut

--- src/fifth.rs
+++ src/fifth.rs
@@ -6,7 +6,7 @@ pub struct List<T> {
     tail: *mut Node<T>,
 }
 
-type Link<T> = Option<Box<Node<T>>>;
+type Link<T> = *mut Node<T>;
 
 struct Node<T> {
     elem: T,
@@ -16,37 +16,40 @@ struct Node<T> {
 impl<T> List<T> {
     pub fn new() -> Self {
         List {
-            head: None,
+            head: ptr::null_mut(),
             tail: ptr::null_mut(),
         }
     }
 
     pub fn push(&mut self, elem: T) {
-        let mut new_tail = Box::new(Node { elem, next: None });
-        let raw_tail: *mut _ = &mut *new_tail;
+        unsafe {
+            let mut new_tail = Box::into_raw(Box::new(Node { elem, next: ptr::null_mut() }));
 
-        if !self.tail.is_null() {
-            unsafe {
-                (*self.tail).next = Some(new_tail);
+            if !self.tail.is_null() {
+                (*self.tail).next = new_tail;
+            } else {
+                self.head = new_tail;
             }
-        } else {
-            self.head = Some(new_tail);
-        }
 
-        self.tail = raw_tail;
+            self.tail = new_tail;
+        }
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        self.head.take().map(|head| {
-            let head = *head;
-            self.head = head.next;
+        unsafe {
+            if self.head.is_null() {
+                None
+            } else {
+                let head = Box::from_raw(self.head);
+                self.head = head.next;
 
-            if self.head.is_none() {
-                self.tail = ptr::null_mut();
+                if self.head.is_null() {
+                    self.tail = ptr::null_mut();
+                }
+
+                Some(head.elem)
             }
-
-            head.elem
-        })
+        }
     }
 }
 
