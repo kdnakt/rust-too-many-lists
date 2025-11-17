7ff0890e91dcd3325d34e884c56db086e0e010ec
kdnakt <a.kid.1985@gmail.com>
2025-11-18 00:25:54 +0900

    push and pop front

--- src/sixth.rs
+++ src/sixth.rs
@@ -24,6 +24,50 @@ impl<T> LinkedList<T> {
             _marker: PhantomData,
         }
     }
+
+    pub fn push_front(&mut self, elem: T) {
+        unsafe {
+            let new = NonNull::new_unchecked(Box::into_raw(Box::new(Node {
+                front: None,
+                back: None,
+                elem,
+            })));
+            if let Some(old) = self.front {
+                (*old.as_ptr()).front = Some(new);
+                (*new.as_ptr()).back = Some(old);
+            } else {
+                debug_assert!(self.back.is_none());
+                debug_assert!(self.front.is_none());
+                debug_assert!(self.len == 0);
+                self.back = Some(new);
+            }
+            self.front = Some(new);
+            self.len += 1;
+        }
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        unsafe {
+            self.front.map(|node| {
+                let boxed_node = Box::from_raw(node.as_ptr());
+                let result = boxed_node.elem;
+
+                self.front = boxed_node.back;
+                if let Some(new_front) = self.front {
+                    (*new_front.as_ptr()).front = None;
+                } else {
+                    debug_assert!(self.len == 1);
+                    self.back = None;
+                }
+                self.len -= 1;
+                result
+            })
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
 }
 
 #[cfg(test)]
@@ -32,5 +76,39 @@ mod tests {
     #[test]
     fn it_works() {
         let mut list: LinkedList<i32> = LinkedList::new();
+    }
+
+    #[test]
+    fn test_front() {
+        let mut list = LinkedList::new();
+        assert_eq!(list.len(), 0);
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.len(), 0);
+
+        list.push_front(10);
+        assert_eq!(list.len(), 1);
+        assert_eq!(list.pop_front(), Some(10));
+        assert_eq!(list.len(), 0);
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.len(), 0);
+
+        list.push_front(20);
+        assert_eq!(list.len(), 1);
+        list.push_front(30);
+        assert_eq!(list.len(), 2);
+        list.push_front(40);
+        assert_eq!(list.len(), 3);
+        assert_eq!(list.pop_front(), Some(40));
+        assert_eq!(list.len(), 2);
+        list.push_front(50);
+        assert_eq!(list.len(), 3);
+        assert_eq!(list.pop_front(), Some(50));
+        assert_eq!(list.len(), 2);
+        assert_eq!(list.pop_front(), Some(30));
+        assert_eq!(list.len(), 1);
+        assert_eq!(list.pop_front(), Some(20));
+        assert_eq!(list.len(), 0);
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.len(), 0);
     }
 }
