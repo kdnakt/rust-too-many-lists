0e98cbe4cd1d2409bf2d1d46d9fd6711a9447f15
kdnakt <a.kid.1985@gmail.com>
2025-12-03 23:49:19 -0800

    test debug

--- src/sixth.rs
+++ src/sixth.rs
@@ -614,4 +614,14 @@ mod tests {
         assert!(!(s <= t));
         assert!(s >= t);
     }
+
+    #[test]
+    fn test_debug() {
+        let list: LinkedList<i32> = (0..10).collect();
+        assert_eq!(format!("{:?}", list), "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]");
+
+        let list: LinkedList<&str> = vec!["just", "one", "test", "more"]
+        .iter().copied().collect();
+        assert_eq!(format!("{:?}", list), r#"["just", "one", "test", "more"]"#);
+    }
 }
