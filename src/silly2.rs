4622b71495d931fced0367ac87494382913d9707
kdnakt <a.kid.1985@gmail.com>
2025-12-29 00:48:31 +0900

    List::push()

--- src/silly2.rs
+++ src/silly2.rs
@@ -2,3 +2,29 @@ pub struct List<'a, T> {
     pub data: T,
     pub prev: Option<&'a List<'a, T>>,
 }
+
+impl<'a, T> List<'a, T> {
+    pub fn push<U>(
+        prev: Option<&'a List<'a, T>>,
+        data: T,
+        callback: impl FnOnce(&List<'a, T>) -> U,
+    ) -> U {
+        let list = List { data, prev };
+        callback(&list)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn basic() {
+        List::push(None, 3, |list| {
+            assert_eq!(list.data, 3);
+            List::push(Some(list), 5, |list| {
+                assert_eq!(list.data, 5);
+                assert_eq!(list.prev.unwrap().data, 3);
+            });
+        });
+    }
+}
