e68c1ea1c87096a47a442ebdca128f1a17e2dc88
kdnakt <a.kid.1985@gmail.com>
2025-12-30 01:06:50 +0900

    impl Iter

--- src/silly2.rs
+++ src/silly2.rs
@@ -12,6 +12,25 @@ impl<'a, T> List<'a, T> {
         let list = List { data, prev };
         callback(&list)
     }
+
+    pub fn iter(&'a self) -> Iter<'a, T> {
+        Iter { next: Some(self) }
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a List<'a, T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.prev;
+            &node.data
+        })
+    }
 }
 
 #[cfg(test)]
@@ -24,6 +43,19 @@ mod tests {
             List::push(Some(list), 5, |list| {
                 assert_eq!(list.data, 5);
                 assert_eq!(list.prev.unwrap().data, 3);
+            });
+        });
+    }
+
+    #[test]
+    fn iter() {
+        List::push(None, 3, |list| {
+            assert_eq!(list.iter().copied().sum::<i32>(), 3);
+            List::push(Some(list), 5, |list| {
+                assert_eq!(list.iter().copied().sum::<i32>(), 8);
+                List::push(Some(list), 11, |list| {
+                    assert_eq!(list.iter().copied().sum::<i32>(), 19);
+                });
             });
         });
     }
